aaf81ba3585380253997392675dfa91c45d7e772
Bob Gardner <>
2023-03-12 11:06:33 -0400

    Rename ci-default to ci-portable

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -16,6 +16,6 @@ subprocess.run(
 subprocess.run(["git", "add", "*"], check=True)
 subprocess.run(["git", "commit", "-m", "Add files"], check=True)
 subprocess.run(["just", "setup", "fmt"], check=True)
-subprocess.run(["cmake", "--preset", "ci-default"], check=True)
-pathlib.Path("compile_commands.json").symlink_to("build/ci-default/compile_commands.json")
+subprocess.run(["cmake", "--preset", "ci-portable"], check=True)
+pathlib.Path("compile_commands.json").symlink_to("build/ci-portable/compile_commands.json")
 subprocess.run(["git", "commit", "--all", "--amend", "--no-edit"], check=True)
