8f3481ae142b58a91b11b88151dcc90c50b2833b
Bob Gardner <bob.hn.gardner@gmail.com>
2023-06-09 16:44:26 -0400

    Disable post-gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,3 @@
-import pathlib
 import subprocess
 
 subprocess.run(["git", "init"], check=True)
@@ -15,7 +14,3 @@ subprocess.run(
 )
 subprocess.run(["git", "add", "*"], check=True)
 subprocess.run(["git", "commit", "-m", "Add files"], check=True)
-subprocess.run(["just", "setup", "fmt"], check=True)
-subprocess.run(["cmake", "--preset", "ci-portable"], check=True)
-pathlib.Path("compile_commands.json").symlink_to("build/ci-portable/compile_commands.json")
-subprocess.run(["git", "commit", "--all", "--amend", "--no-edit"], check=True)
